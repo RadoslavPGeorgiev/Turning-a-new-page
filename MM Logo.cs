a9cd5340205c67fc4eb86205b82f97028ddb3a2c
RadoslavPGeorgiev <50392748+RadoslavPGeorgiev@users.noreply.github.com>
2019-05-08 14:28:05 +0300

    Update MM Logo.cs

--- MM Logo.cs
+++ MM Logo.cs
@@ -1,66 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp8
 {
     class Program
     {
-        static int k;
+        static int k, count = 0,count2 = 0;
+        static void TopHalfOfM(int Size, int i)
+        {
+            Minuses(Size - i);
+            Stars(Size + (i * 2));
+            Minuses(Size - (i * 2));
+            Stars(Size + (i * 2));
+            Minuses(Size - i);
+        }
+        static void BottomHalfOfM(int Size,int i,int j)
+        {
+            if (j == 0)
+            {
+                Minuses(Size - i);
+                Stars(Size);
+                Minuses(1 + count);
+                Stars((Size * 2) - 1 - count);
+                Minuses(1 + count);
+                Stars(Size);
+                Minuses(Size - i);
+                count += 2;
+            }
+            else
+            {
+                Minuses(Size - i);
+                Stars(Size);
+                Minuses(1 + count2);
+                Stars((Size * 2) - 1 - count2);
+                Minuses(1 + count2);
+                Stars(Size);
+                Minuses(Size - i);
+                count2 += 2;
+            }
+        }
         static void Stars(int number)
         {
             for (k = 0; k < number; k++)
                 Console.Write("*");
         }
         static void Minuses(int number)
-        { 
+        {
             for (k = 0; k < number; k++)
                 Console.Write("-");
         }
         static void Main(string[] args)
         {
             Console.Write("N=");
-            int count = 0, count1 = 0;
-            int n = int.Parse(Console.ReadLine());
-           
-            for (int i = 0; i <= n; i++)
+            int Size = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= Size; i++)
             {
                 for (int j = 0; j < 2; j++)
                 {
-
-                    if (((n + 1) / 2 >= i) && (n > (i * 2)))
+                    if (((Size + 1) / 2 >= i) && (Size > (i * 2)))
                     {
-                        Minuses(n - i);
-                        Stars(n + (i * 2));
-                        Minuses(n - (i * 2));
-                        Stars(n + (i * 2));
-                        Minuses(n - i);
-                    }
-                    else if (j == 0)
-                    {
-                        Minuses(n - i);
-                        Stars(n);
-                        Minuses(1 + count);
-                        Stars((n * 2) - 1 - count);
-                        Minuses(1 + count);
-                        Stars(n);
-                        Minuses(n - i);
-                        count += 2;
+                        TopHalfOfM(Size, i);
                     }
                     else
                     {
-                        Minuses(n - i);
-                        Stars(n);
-                        Minuses(1 + count1);
-                        Stars((n * 2) - 1 - count1);
-                        Minuses(1 + count1);
-                        Stars(n);
-                        Minuses(n - i);
-                        count1 += 2;                      
-                    }
-                }       
+                        BottomHalfOfM(Size, i, j);
+                    }   
+                }
                 Console.WriteLine();
             }
             Console.ReadLine();
